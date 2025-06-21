dec5c6e8ae12fb428a3524e27649d097d4c67953
ShahdAhmed <shahdahmed851@gmail.com>
2025-06-21 23:34:16 +0300

    Editing Email Sender

--- app/core/config.py
+++ app/core/config.py
@@ -4,8 +4,8 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 SMTP_HOST = "smtp.gmail.com"
 SMTP_PORT = 587
-SMTP_USER = "mennatallahahmed892@gmail.com"
-SMTP_PASSWORD = "zpqj rhus eubt lgrz"
+SMTP_USER = "fluently567@gmail.com"
+SMTP_PASSWORD = "paby vywt benh ziui"
 EMAIL_FROM = "noreply@gmail.com"
 BASE_URL = "http://localhost:8000"
 
